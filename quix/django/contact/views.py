c2ec35312f352d756a298a31ffea72d796c688f2
st <stondo@gmail.com>
2013-03-21 17:38:42 +0100

    request

--- quix/django/contact/views.py
+++ quix/django/contact/views.py
@@ -3,19 +3,17 @@ from django.core.urlresolvers import reverse_lazy
 from django.views.generic.edit import FormView
 from django.utils.importlib import import_module
 
-from django.http import HttpRequest
-
 # import contact form class based on value in settings.py
 full_class = getattr(settings, 'CONTACT_FORM_CLASS', 'quix.django.contact.forms.ContactForm')
 module_name = '.'.join(full_class.split('.')[0:-1])
 module = import_module(module_name)
 class_instance = getattr(module, full_class.split('.')[-1])
 
-class ContactView(FormView):
-    if HttpRequest.session['flavour'] == 'full':
+class ContactView(request, FormView):
+    if request.session['flavour'] == 'full':
         template_name = getattr(settings, 'CONTACT_FORM_TEMPLATE', 'contact/form.html')
         success_url = reverse_lazy("contact-success")
-    elif HttpRequest.session['flavour'] == 'mobile':
+    elif request.session['flavour'] == 'mobile':
         template_name = getattr(settings, 'CONTACT_FORM_MOBILE_TEMPLATE', 'contact/mobileForm.html')
         success_url = reverse_lazy("contact-mobileSuccess")
     else:
