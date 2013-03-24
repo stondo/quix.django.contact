3aad61eb67d8c1e5ae1fc58f51e9c0f6b9ba51c9
st <stondo@gmail.com>
2013-03-24 18:26:31 +0100

    urls modified (mobile)

--- quix/django/contact/urls.py
+++ quix/django/contact/urls.py
@@ -3,10 +3,16 @@ from django.conf.urls.defaults import patterns, url
 from quix.django.contact.views import ContactView
 from django.views.generic import TemplateView
 
-template_name = getattr(settings, 'CONTACT_SUCCESS_TEMPLATE', 'contact/success.html')
+full_template_name = getattr(settings, 'CONTACT_SUCCESS_TEMPLATE', 'contact/success.html')
+
+mobile_template_name = getattr(settings, 'CONTACT_SUCCESS_MOBILE_TEMPLATE', 'contact/mobileSuccess.html')
+
 
 urlpatterns = patterns('',
     url(r'^$', ContactView.as_view(), name="contact-form"),
     url(r'^success/$', TemplateView.as_view(template_name=template_name),
         name="contact-success"),
+    url(r'^mobilesuccess/$', TemplateView.as_view(template_name=template_name),
+        name="contact-mobileSuccess"),
+
 )
